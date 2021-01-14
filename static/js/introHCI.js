fd6c196d8a5cccf1b2757b422034b2ed1698d052
Steven Shoemaker <stshoema@ucsd.edu>
2021-01-13 22:06:06 -0800

    added Javascript!

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -7,3 +7,10 @@ function initializePage() {
             $('.jumbotron h1').text("Javascript is connected");
             });
 }
+
+$("a.thumbnail").click(projectClick)
+
+function projectClick(e) {
+    e.preventDefault();
+    $(this).css("background-color", "#7fff00");
+}
